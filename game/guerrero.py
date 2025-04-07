3c6735765734ea4b7cb98b4d943c68876df33018
RenzoPS <renzopiris2006@gmail.com>
2025-04-06 23:24:17 -0300

    Delete game directory

--- game/guerrero.py
+++
@@ -1,7 +0,0 @@
-from personaje import Personaje
-
-class Guerrero(Personaje):
-    def __init__(self, nombre):
-        super().__init__(nombre)
-        self.defensa += self.defensa * 1.05
-        self.ataque -= 3
--- game/mago.py
+++
@@ -1,8 +0,0 @@
-from personaje import Personaje
-class Mago(Personaje):
-    def __init__(self, nombre):
-        super().__init__(nombre)
-        self.ataque += self.ataque * 1.07
-        self.defensa -= 7
-        if self.defensa < 0:
-            self.defensa = 0
